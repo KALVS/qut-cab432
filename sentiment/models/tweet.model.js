939a512dfe426b4608a08c03d9d2575e2f394699
Tylor Stewart <trjstewart@gmail.com>
2016-09-19 14:20:04 +1000

    Nuked Sentiment Microservice

--- sentiment/models/tweet.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-// Tweet Schema Definition.
-tweetSchema = new Schema( { tweet: {}, sentiment: { type: Number, default: null } });
-
-// Construct and Export the Model.
-module.exports = mongoose.model('tweet', tweetSchema);
