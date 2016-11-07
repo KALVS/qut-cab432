217427357f89691ac7e4a649820ae7a79b1d87f0
AshleyMcVeigh <ash22334@hotmail.com>
2016-11-07 18:21:12 +1000

    fixed word cloud

--- Assignment 2/client/routes/index.js
+++ Assignment 2/client/routes/index.js
@@ -3,29 +3,30 @@
 const router = require('express').Router();
 const models = require('../models');
 
+
 router.get('/', (req, res) => {
   Promise.all([
     models.Tweet.aggregate('sentiment', 'count'),
     models.Tweet.aggregate('sentiment', 'avg', { dataType: 'float' }),
     models.Word.findOne({ where: { word: 'literally' }}),
     models.Word.findOne({ where: { word: 'figuratively' }}),
-    models.Word.findAll({ order: 'count DESC', limit: 50 })
+    models.Word.findAll({ order: 'count DESC', limit: 500 })
   ]).then(results => {
     // Now we have our data, we need it in a nicer format.
     const totalTweets = results[0];
     const globalSentiment = results[1];
     const defcon = (globalSentiment < -1.5) ? 1 : (globalSentiment >= -1.5 && globalSentiment < -0.5) ? 2 : (globalSentiment >= -0.5 && globalSentiment < 0.5) ? 3 : (globalSentiment >= 0.5 && globalSentiment < 1.5) ? 4 : 5;
     const wordVsWord = Math.floor((results[2].count / (results[2].count + results[3].count)) * 100);
-    const wordCloud = [['foo', 10], ['bar', 6], ['lol', 6], ['poo', 6], ['yup', 6]];
-    // const wordCloud = results[4].map(word => [word.word, word.count]);
+    const wordCloud = results[4].map(word => [word.word, Math.round((word.count) /20)]);
     const data = {
       totalTweets: totalTweets,
       globalSentiment: globalSentiment,
       defcon: defcon,
       wordVsWord: wordVsWord,
-      wordCloud: wordCloud
+      wordCloud: JSON.stringify(wordCloud)
     };
 
+    console.log(JSON.stringify(wordCloud));
     res.render('index', data);
   });
 });
